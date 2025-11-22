8af6287cb5bded9fbd3c44cfa2901cb3416ca1fc
Marcos-Elias96 <233241033@alumnos.utn.edu.mx>
2025-11-22 02:25:49 -0600

    Agregar modelo User.js requerido

--- models/User.js
+++ models/User.js
@@ -1,17 +1,13 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  usuario: { type: String, required: true, unique: true }, // matrícula o correo
+  usuario: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  rol: { type: String, enum: ["alumno", "profesor"], required: true },
 
-  matricula: { type: String, default: null },
+  rol: { type: String, required: true },
+
   correo: { type: String, default: null },
-
-  fechaRegistro: {
-    type: Date,
-    default: Date.now
-  }
+  matricula: { type: String, default: null }
 });
 
 module.exports = mongoose.model("User", UserSchema);
